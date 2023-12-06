4d574971f3e7f99a7420c3d879aa294199dbace8
maximumvvoltage <004kps@gmail.com>
2023-12-06 18:48:06 +0000

    VSF DA!!!!!!! BABABABABABA

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D rb;
+    // Use this for initialization
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        float speed = 4.0f;
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal"),
+       Input.GetAxis("Vertical")) * speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
