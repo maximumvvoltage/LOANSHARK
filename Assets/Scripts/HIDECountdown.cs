36cfb6dce04eee85d250c5bd08fabfca7fa58d15
maximumvvoltage <004kps@gmail.com>
2024-01-21 19:38:29 +0000

    timer and dialogue complete!

--- Assets/Scripts/HIDECountdown.cs
+++ Assets/Scripts/HIDECountdown.cs
@@ -7,7 +7,7 @@ public class HIDECountdown : MonoBehaviour
 {
     //float seekingStartTime;
     public float hidingStartTime = 30f;
-    public float seekingStartTime = 190.1f;
+    public float seekingStartTime = 160.1f;
 
     [Header("GameObjects")]
     public GameObject HideObject;
@@ -15,7 +15,10 @@ public class HIDECountdown : MonoBehaviour
     public GameObject darkness;
 
     [Header("Dialogue Boxes")]
+    public GameObject DieDialogue;
+    public GameObject FindDialogue;
     public GameObject startDialogue;
+    public GameObject sixtySeconds; 
     public GameObject halfTime;
     public GameObject loseDialogue;
     public GameObject winDialogue;
@@ -48,11 +51,29 @@ public class HIDECountdown : MonoBehaviour
             HideText.color = Color.red;
         }
 
+        if (hidingStartTime <= 30)
+        {
+            DieDialogue.SetActive(true);
+            if (hidingStartTime <= 27)
+            {
+                DieDialogue.SetActive(false);
+            }    
+        }
+
+        if (hidingStartTime <= 27)
+        {
+            FindDialogue.SetActive(true);
+            if (hidingStartTime <= 24)
+            {
+                FindDialogue.SetActive(false);
+            }    
+        }
         if (hidingStartTime <= 0)
         {
             HideObject.SetActive(false);
             darkness.SetActive(true);
             SeekObject.SetActive(true);
+            hidingStartTime = 0;
         }
     }
 
@@ -68,18 +89,26 @@ public class HIDECountdown : MonoBehaviour
             SeekText.color = Color.green;
         }
 
-        if (seekingStartTime <= 117)
+        if (seekingStartTime <= 129)
         {
             startDialogue.SetActive(true);
-            if (seekingStartTime <= 115)
+            if (seekingStartTime <= 124)
             {
                 startDialogue.SetActive(false);
             }    
         }
+        if (seekingStartTime <= 60)
+        {
+            sixtySeconds.SetActive(true);
+            if (seekingStartTime <= 53)
+            {
+                sixtySeconds.SetActive(false);
+            }
+        }
         if (seekingStartTime <= 30)
         {
             halfTime.SetActive(true);
-            if (seekingStartTime <= 27)
+            if (seekingStartTime <= 23)
             {
                 halfTime.SetActive(false);
             }
